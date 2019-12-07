adacab8134303627583948b54a0ca834d0a7512e
NNShadow <1581333251@qq.com>
2019-12-07 10:15:07 +0800

    过滤

--- client/src/main/java/com/npy/ClientApplication.java
+++ client/src/main/java/com/npy/ClientApplication.java
@@ -2,6 +2,7 @@ package com.npy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@ServletComponentScan
 public class ClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
--- client/src/main/java/com/npy/filter/UserFilter.java
+++ client/src/main/java/com/npy/filter/UserFilter.java
@@ -15,8 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-//@Component
-//@WebFilter(urlPatterns = {"/index.html","/account/redirect/index","/order.html","/account/redirect/order"},filterName = "userFilter")
+@Component
+@WebFilter(urlPatterns = {"/index.html","/account/redirect/index","/order.html","/account/redirect/order"},filterName = "userFilter")
 public class UserFilter implements Filter {
 
     @Override
